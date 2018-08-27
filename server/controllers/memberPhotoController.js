49397dd59efc364f2cb01ac78d47dfa5cd8b2082
James U <jamesu@MacBook-Pro-13.local>
2018-08-27 23:09:58 +1000

    Add logic to use either .jpg or .JPG

--- server/controllers/memberPhotoController.js
+++ server/controllers/memberPhotoController.js
@@ -4,16 +4,25 @@ module.exports = {
 
   // get photo by member by ID
   getByMemberId: function(req, res) {
-    // image paths
-    var thumbnail = __dirname + '/../../photos/thumbnails/' + req.params.memberId + '.jpg';
-    var original = __dirname + '/../../photos/' + req.params.memberId + '.jpg';
+    // image base paths
+    var thumbnail = __dirname + '/../../photos/thumbnails/' + req.params.memberId;
+    var original = __dirname + '/../../photos/' + req.params.memberId;
 
     // get thumbnail if exists, else original
-    if (fs.existsSync(thumbnail)) {
+    // try both JPG and jpg
+    if (fs.existsSync(thumbnail + '.JPG')) {
+      res.sendFile(req.params.memberId + '.JPG', {
+        root: __dirname + '/../../photos/thumbnails'
+      });
+    } else if (fs.existsSync(thumbnail + '.jpg')) {
       res.sendFile(req.params.memberId + '.jpg', {
         root: __dirname + '/../../photos/thumbnails'
       });
-    } else if (fs.existsSync(original)) {
+    } else if (fs.existsSync(original + '.JPG')) {
+      res.sendFile(req.params.memberId + '.JPG', {
+        root: __dirname + '/../../photos'
+      });
+    } else if (fs.existsSync(original + '.jpg')) {
       res.sendFile(req.params.memberId + '.jpg', {
         root: __dirname + '/../../photos'
       });
