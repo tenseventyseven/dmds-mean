daf1b9e2449f1b887958c8c0794c6c2b5250d0a0
James U <tenseventyseven@gmail.com>
2018-08-27 23:18:31 +1000

    Also handle JPG files

--- scripts/create_thumbnails.sh
+++ scripts/create_thumbnails.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+set -x
 
 # Create thumbnail versions of ID photos
 cd /opt/dmds-mean/photos
-for f in *.jpg; do
+for f in *.jpg *.JPG; do
 	# if thumbnail doesn't already exist
 	if [ ! -f thumbnails/$f ]; then
 		convert $f -resize 300x300\> thumbnails/$f
