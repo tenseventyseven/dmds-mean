aa60da85f8fd2f8f33869c1214f93452b89defc3
James U <tenseventyseven@gmail.com>
2018-08-28 21:42:09 +1000

    Add file extension adding logic

--- scripts/create_thumbnails.sh
+++ scripts/create_thumbnails.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
-# Create thumbnail versions of ID photos
 cd /opt/dmds-mean/photos
+
+# Add jpg extension if missing
+for f in *; do
+	ext=`echo $f | awk -F . '{print $NF}'`
+	if [ "$ext" != "JPG" -a "$ext" != "jpg" -a "$ext" != "thumbnails" ]; then
+		# Add file extension
+		mv "$f" "${f}.jpg"
+	fi
+done
+
+# Create thumbnail versions of ID photos
 for f in *.jpg *.JPG; do
 	# if thumbnail doesn't already exist
 	if [ ! -f thumbnails/$f ]; then
