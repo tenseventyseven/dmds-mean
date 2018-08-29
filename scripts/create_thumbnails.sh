a4761e4ca2c67eaf6c70c9c7bcad16a6f53002d0
James U <tenseventyseven@gmail.com>
2018-08-29 18:53:39 +1000

    Simplify logic

--- scripts/create_thumbnails.sh
+++ scripts/create_thumbnails.sh
@@ -2,20 +2,13 @@
 
 cd /opt/dmds-mean/photos
 
-# Add jpg extension if missing
-for f in *; do
-	ext=`echo $f | awk -F . '{print $NF}'`
-	if [ "$ext" != "JPG" -a "$ext" != "jpg" -a "$ext" != "thumbnails" ]; then
-		# Add file extension
-		mv "$f" "${f}.jpg"
-	fi
-done
-
 # Create thumbnail versions of ID photos
-for f in *.jpg *.JPG; do
-	# if thumbnail doesn't already exist
-	if [ ! -f thumbnails/$f ]; then
-		convert $f -resize 300x300\> thumbnails/$f
+for f in *; do
+	if [ -f $f ]; then
+		# if thumbnail doesn't already exist
+		if [ ! -f thumbnails/$f ]; then
+			convert $f -resize 300x300\> thumbnails/$f
+		fi
 	fi
 done
 
